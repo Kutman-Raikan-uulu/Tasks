f0a0551ce6e44b77d69ef2808dbdc3656ad2fa07
Kutman Raikan uulu <rajkanuulu@gmail.com>
2020-07-09 01:55:51 +0600

    initial commit

---
+++ taskmanager/main/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+# Create your views here.
+def index(request):
+    tasks = Task.objects.order_by('title')
+    return render(request, 'main/index.html', {'title': 'Home page', 'tasks': tasks})
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Forms was wrong'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+
+
